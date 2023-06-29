90b1ed99f87bf230703e6705af646d946d53dbbf
edumartim <edumartim@gmail.com>
2023-06-29 12:31:39 -0300

    implementando details (HTML,CSS,JS)

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,21 +1,83 @@
-const pokemonDetailsContainer = document.getElementById("pokemonDetails");
+//----busca o pokemon aleatório na API-----//
+const urlApi = "https://pokeapi.co/api/v2/pokemon";
+const pokemonElement = document.querySelector("div.pokemon"); // seleciona a div pokemon lá do html
 
-function renderPokemonDetails(pokemon) {
-  const html = `
-    <h2>${pokemon.name}</h2>
-    <p>Height: ${pokemon.height}</p>
-    <p>Weight: ${pokemon.weight}</p>
-    <p>Ability: ${pokemon.ability}</p>
-    <img src="${pokemon.photo}" alt="${pokemon.name}">
-    `;
-    pokemonDetailsContainer.innerHTML = html;
-  }
-  
-  function fetchPokemonDetails(pokemonId) {
-    pokeApi
-    .getPokemonDetail(pokemonId)
-    .then((pokemon) => renderPokemonDetails(pokemon))
-    .catch((error) => console.log(error));
-  }
+const random = () => Math.floor(Math.random() * 150);
 
+const getAbilities = (abilities) => abilities.map((item) => item.ability.name);
 
+const createAbilities = (abilities) =>
+  abilities.reduce((acc, item) => (acc += `<li>${item}</li>`), "");
+
+// aqui esta sendo injetado o html que estava na div pokemon
+const createPokemon = ({ image, id, name, abilities, weight, height }) => {
+  pokemonElement.innerHTML = `
+    <div class="pokemon__wrapperImage"><img src="${image}" class="pokemon__image" alt="pokemon ${name}" /></div>
+      <div class="pokemon__info">
+        <h1>Details</h1>
+        <h2 class="pokemon__name">
+          #${id}
+          ${name}
+        </h2>
+        <h3>Abilities:</h3>
+          <ul class="pokemon__details">${createAbilities(abilities)}</ul>
+          <ul><h3>Weight: ${weight} </h3></ul>
+          <ul><h3>Height: ${height}</h3></ul>
+      </div>
+  `;
+};
+
+const getPokemon = () =>
+  fetch(`${urlApi}/${random()}`)
+    .then((response) => response.json())
+    .then(
+      ({
+        id,
+        name,
+        sprites: {
+          other: {
+            dream_world: { front_default: image },
+          },
+        },
+        height,
+        weight,
+        abilities,
+      }) => {
+        const pokemonSelected = {
+          id,
+          name,
+          image,
+          height,
+          weight,
+          abilities: getAbilities(abilities),
+        };
+        createPokemon(pokemonSelected);
+      }
+    );
+
+getPokemon();
+
+/*
+const pokemon__features = ({ height, weight }) => {
+  pokemonElement.innerHTML = `
+  <div class="pokemon__features">
+  <h2>Details</h2>
+    <p class="pokemon__height">Height: ${height}</p>
+    <p class="pokemon__weight">Weight: ${weight}</p>
+  </div>
+  `;
+};
+
+const getPokemonFeatures = () =>
+  fetch(`${urlApi}/${random()}`)
+    .then((response) => response.json())
+    .then(({ height, weight }) => {
+      const pokemonSelected = {
+        height,
+        weight,
+      };
+      pokemon__features(pokemonSelected);
+    });
+
+getPokemonFeatures();
+*/
